1ec3fabcd74cd56f604dd93d01c8c22dac34bdaa
Adriano <adriano.junior@ccc.ufcg.edu.br>
2023-01-10 11:26:02 -0300

    Refactor: query in executeQuery in Program class

--- recover-data/src/application/Program.java
+++ recover-data/src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 
             st = conn.createStatement();
 
-            rs = st.executeQuery("select * from department");
+            rs = st.executeQuery("SELECT * FROM department");
 
             while (rs.next()) {
                 System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
